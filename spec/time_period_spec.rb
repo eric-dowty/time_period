9d48edd893200eb4e7526d9af85c336e950f2cf6
eric-dowty <eric.m.dowty@gmail.com>
2018-08-27 13:57:52 -0600

    Updated variable names in spec file

--- spec/time_period_spec.rb
+++ spec/time_period_spec.rb
@@ -29,57 +29,57 @@ describe TimePeriod do
 
   describe '#intersection' do
     it 'should return empty intersection if intersecting time period ends before time period starts' do
-      intersecting_time_stamp = TimePeriod.new(start_epoch-100, start_epoch-10)
+      intersecting_time_period = TimePeriod.new(start_epoch-100, start_epoch-10)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([])
     end
 
     it 'should return empty intersection if intersecting time period starts after time period ends' do
-      intersecting_time_stamp = TimePeriod.new(finish_epoch+10, finish_epoch+100)
+      intersecting_time_period = TimePeriod.new(finish_epoch+10, finish_epoch+100)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([])
     end
 
     it 'should return start_epoch and finish_epoch if intersecting time period overlaps' do
-      intersecting_time_stamp = TimePeriod.new(start_epoch-10, finish_epoch+10)
+      intersecting_time_period = TimePeriod.new(start_epoch-10, finish_epoch+10)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([start_epoch, finish_epoch])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([start_epoch, finish_epoch])
     end
 
     it 'should return start and end of intersecting time period if it is contained within the time period' do
-      intersecting_time_stamp = TimePeriod.new(start_epoch+10, finish_epoch-10)
+      intersecting_time_period = TimePeriod.new(start_epoch+10, finish_epoch-10)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([start_epoch+10, finish_epoch-10])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([start_epoch+10, finish_epoch-10])
     end
 
     it 'should return start and end of intersecting time period if it starts and ends at the same time' do
-      intersecting_time_stamp = TimePeriod.new(start_epoch, finish_epoch)
+      intersecting_time_period = TimePeriod.new(start_epoch, finish_epoch)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([start_epoch, finish_epoch])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([start_epoch, finish_epoch])
     end
 
     it 'should return the start of the time period and end of the intersecting time period if it ends after the time period start' do
-      intersecting_time_stamp = TimePeriod.new(start_epoch-10, start_epoch+10)
+      intersecting_time_period = TimePeriod.new(start_epoch-10, start_epoch+10)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([start_epoch, start_epoch+10])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([start_epoch, start_epoch+10])
     end
 
     it 'should return the start of the time period and end of the intersecting time period if it ends after the time period start and starts at the same time' do
-      intersecting_time_stamp = TimePeriod.new(start_epoch, start_epoch+10)
+      intersecting_time_period = TimePeriod.new(start_epoch, start_epoch+10)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([start_epoch, start_epoch+10])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([start_epoch, start_epoch+10])
     end
 
     it 'should return the start of the intersecting time period and end of the time period if it starts before the time period finish' do
-      intersecting_time_stamp = TimePeriod.new(finish_epoch-10, finish_epoch+10)
+      intersecting_time_period = TimePeriod.new(finish_epoch-10, finish_epoch+10)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([finish_epoch-10, finish_epoch])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([finish_epoch-10, finish_epoch])
     end
 
     it 'should return the start of the intersecting time period and end of the time period if it starts before the time period finish and ends at the same time' do
-      intersecting_time_stamp = TimePeriod.new(finish_epoch-10, finish_epoch)
+      intersecting_time_period = TimePeriod.new(finish_epoch-10, finish_epoch)
 
-      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_stamp)).to eq([finish_epoch-10, finish_epoch])
+      expect(TimePeriod.new(start_epoch,finish_epoch).intersection(intersecting_time_period)).to eq([finish_epoch-10, finish_epoch])
     end
   end
 end
